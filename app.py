942bc76c3c2df218c8f97fc7adbb2d79372e6d64
leejap <jap2371@naver.com>
2025-07-07 17:22:26 +0900

    두번표기 지우기

--- app.py
+++ app.py
@@ -82,14 +82,16 @@ def character_equipment():
 
         for item in equip_data:
             grade = item.get("Grade", "")
-            part = item.get("Type", "")            
-            quality = item.get("qualityValue", "" , 0)
+            part = item.get("Type", "")
+            
+            quality = item.get("qualityValue", 0)
             refine = item.get("TinkerLevel", "10단계")
             name = item.get("Name", "")
 
-            
+            if "+" in name:
+                Value = "+" + name.split("+")[1].split()[0]
 
-            message += f"[{grade} {part}] +{name} / 품질 : {quality} / 상급재련 : {refine}\n"
+            message += f"[{grade} {part}] + {name} / 품질 : {quality} / 상급재련 : {refine}\n"
 
 
             if part in["무기", "투구", "상의", "하의", "장갑", "어깨"]:
